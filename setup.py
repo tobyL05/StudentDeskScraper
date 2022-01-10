c098a14c1e054a8eced69e9aafa96df85c9131c2
tobyL05 <toby123mc@gmail.com>
2022-01-10 20:26:00 +0700

    fixed setup

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-import subprocess
 import os
 
-print("Running startup")
-path_to_req = os.getcwd() + "\\requirements.txt"
-cmd = f"pip install -r {path_to_req}"
-try:
-	subprocess.run(args=cmd)
-except:
-	print("Error downloading modules")
-from src.login import start
-start()
+def reqs():
+	print("Running startup")
+	path_to_req = os.path.dirname(__file__) + "\\requirements.txt"
+	cmd = f"pip install -r {path_to_req}"
+	os.system(cmd)
+
+
+if __name__ == "__main__":
+	reqs()
+	from src.login import start
+	start()
